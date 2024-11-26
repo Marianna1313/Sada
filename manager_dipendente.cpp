3e197e7f9e22b6e5f43f479764ca3f0ab2c4e4c2
Marianna1313 <marianna.sada@itctosi.va.it>
2024-11-26 11:50:51 +0000

    continuazione manager

--- manager_dipendente.cpp
+++ manager_dipendente.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include <cmath>
 using namespace std;
 
 class Dipendente {
@@ -12,7 +13,7 @@ class Dipendente {
         stipendioBase=s;
     }
     void visualizza(){
-        cout <<"Il dipendente si chiama " << nome << "e il suo stipendio è " << stipendioBase;
+        cout <<"Il dipendente si chiama " << nome << " e il suo stipendio è " << stipendioBase<<endl;
     }
     double totale_salario(double b){
         double tot=0;
@@ -23,10 +24,53 @@ class Dipendente {
         }
         return tot;
     }
+    double giorni_lavoro (double g){
+        double stipl=0;
+        stipl=stipendioBase/g;
+        stipl = round(stipl * 100) / 100.0;
+        return stipl;
+    }
 
 };
+    class manager: public Dipendente{
+        private: 
+        double bonus;
+        int numeroDipendenti;
+        public:
+        manager (string n, double s, double bs, int nd): Dipendente(n,s){
+            bonus = bs;
+            numeroDipendenti=nd;
+        }
+        void visualizza (){
+            cout <<"Il bonus del manager: " << bonus << " e ha " << numeroDipendenti<< " dipendenti " <<endl;
+        }
+    };
 
 
 int main(){
+    string n;
+    double s;
+    double b,g=0,bs;
+    int nd;
     
-}
+    
+    cout << "Inserisci il nome: ";
+    cin >> n;
+    cout << "Inserisci lo stipendio base: ";
+    cin >> s;
+    Dipendente x=Dipendente(n,s);
+    x.visualizza();
+    cout << "Se c'è un bonus inseriscilo, altrimenti digita 0: ";
+    cin >> b;
+    cout<< "Il totale è: " << x.totale_salario(b)<<endl;
+    cout << "Quanti giorni ha lavorato? ";
+    cin >> g;
+    cout<< "Lo stipendio per ogni giorno lavorativo è: : " << x.giorni_lavoro(g);
+    cout << "Quant'è il bonus del manager? ";
+    cin >> bs;
+    cout << "Quanti dipendenti ha il mangaer? ";
+    cin >> nd;
+    manager y = manager (n, s, bs, nd);
+    y.visualizza();
+
+    }
